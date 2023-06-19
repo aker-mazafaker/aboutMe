b90643f2120f314f22c9b525a981936799a7d454
Aker <@akerke.n@inbox.ru>
2023-06-19 13:12:42 +0600

    Initial Commit

---
+++ aboutMe/aboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutMeApp.swift
+//  aboutMe
+//
+//  Created by Akerke on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct aboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
